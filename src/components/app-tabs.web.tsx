3777c7e6fbb124a44b558d2ab6ddc50506a18cc4
Andres <beco20@yahoo.com.ar>
2026-07-21 10:51:42 -0300

    Align reanimated for Expo Go and remove unused explore route

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -24,8 +24,8 @@ export default function AppTabs() {
           <TabTrigger name="home" href="/" asChild>
             <TabButton>Home</TabButton>
           </TabTrigger>
-          <TabTrigger name="explore" href="/explore" asChild>
-            <TabButton>Explore</TabButton>
+          <TabTrigger name="inicio" href="/inicio" asChild>
+            <TabButton>Inicio</TabButton>
           </TabTrigger>
         </CustomTabList>
       </TabList>
